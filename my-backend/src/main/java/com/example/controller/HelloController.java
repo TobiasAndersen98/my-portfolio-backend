d4b540f1139a4c94f686ec54d3c8f61f520a7c94
karl Tobias Nymann Andersen <karl.tobias.andersen@gmail.com>
2025-09-08 17:25:53 +0200

    now it works whit spring boot

--- my-backend/src/main/java/com/example/controller/HelloController.java
+++ my-backend/src/main/java/com/example/controller/HelloController.java
@@ -1,13 +1,12 @@
-java
+package com.example;
 
-package com.example.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Mycontroller {
-    public static void main(String[] args) {
-        SpringApplication.run(Mycontroller.class, args);
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String sayHello() {
+        return "Hello from Java backend 🚀";
     }
 }
