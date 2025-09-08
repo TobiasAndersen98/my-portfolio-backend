f41fd898519338762e148895cf8bb9711c1f731c
karl Tobias Nymann Andersen <karl.tobias.andersen@gmail.com>
2025-09-08 16:48:31 +0200

    first commit

---
+++ my-backend/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+package com.example.controller;
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ my-backend/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,13 @@
+java
+
+package com.example.controller;
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mycontroller {
+    public static void main(String[] args) {
+        SpringApplication.run(Mycontroller.class, args);
+    }
+}
