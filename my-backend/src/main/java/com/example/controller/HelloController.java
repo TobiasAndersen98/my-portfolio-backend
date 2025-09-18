4dfcea7aef5d2c9d01577827259de7b136351495
tobiasandersen98 <karl.tobias.andersen@gmail.com>
2025-09-18 14:57:35 +0200

    pom and dependency

--- my-backend/src/main/java/com/example/controller/HelloController.java
+++ my-backend/src/main/java/com/example/controller/HelloController.java
@@ -1,12 +1,26 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+
+
+@Controller
 public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+    
     @GetMapping("/api/hello")
     public String sayHello() {
-        return "Hello from Java backend 🚀";
+       return "Hello from Java backend 🚀";
     }
+    
 }
